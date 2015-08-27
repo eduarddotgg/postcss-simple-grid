bba2be379859813e4fdfbb801abc9b9150bc44c0
Eduard Pocstar <eduard.pocstar@shiwaforce.com>
2015-08-27 14:33:08 +0200

    create row, create column, create grid

--- index.js
+++ index.js
@@ -2,49 +2,60 @@ var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-grid', function (opts) {
     opts = opts || {};
+    
+    function unitParse(str){
+		var returnValue = {};
+		var match = str.match(/\d./);
+		
+		if(str.length) {
+			var value = match[0];
+			var unit = (str.replace(value, '')).trim();
+			returnValue.unit = unit;
+			returnValue.value = parseInt(value, 10);
+		}
+		return returnValue;
+	}
+    
     return function(css, result){
 
-		// css.walkRules(rule_filter, function (rule) {
-		// 	rule.walkDecls(decl_filter, function (decl) {});
-		// });
+        css.walkDecls(function (decl){
+			if(decl.prop === 'create-row'){
+				var selectorName = decl.parent.selector;
+				var rowValue = decl.value;
+				
+				decl.parent.append({prop: 'display', value: 'block'});
+				decl.parent.append({prop: 'margin', value: '0 auto'});
+				decl.parent.append({prop: 'max-width', value: rowValue});
+				decl.parent.append({prop: 'position', value: 'relative'});
+				decl.parent.append({prop: 'width', value: '100%'});
+				
+				// css.insertAfter((selectorName + ':before,' + selectorName + ':after { content: " "; display: table; }' + selectorName + ':after { clear: both;}'));
+				
+				decl.removeSelf();
+			}
+			
+			if(decl.prop === 'create-column'){
+				var selectorName = decl.parent.selector;
+				var colGut = unitParse(decl.value);
+				
+				decl.parent.append({prop: 'float', value: 'left'});
+				decl.parent.append({prop: 'padding', value: '0 ' + (colGut.value/2) + colGut.unit});
+				decl.parent.append({prop: 'position', value: 'relative'});
+				decl.parent.append({prop: 'width', value: '100%'});
 
-		// css.walkRules(function(rule){
-		// 	if (rule.selector === 'grid'){
-		// 		console.log(rule.nodes);
-		// 	}
-		// });
-		// css.walkRules('.column', function (rule) {
-		// 	if (rule.nodes.indexOf('.column-gutter') >= -1){
-		// 		var index = rule.nodes.indexOf('.column-gutter');
-		// 		var test = rule.nodes[0];
-		// 		console.log(test);
-		// 	}
-		// });
-		// css.walkRules('.s-', function (rule) {
-		// 	console.log(rule);
-		// });
-		css.walkDecls(function(decl){
-			if (decl.prop === 'column-type'){
-				var columnType = decl.value;
-				if (columnType === 'regular'){
-					decl.parent.append('float: left; position: relative; width: 100%;');
-				}
-				decl.remove();
+				decl.removeSelf();
 			}
-			if (decl.prop === 'column-gutter'){
-				var columnGutter = decl.value / 2;
-				decl.parent.append('padding: 0 ' + columnGutter + 'px;');
-				decl.remove();
-			}
-			if (decl.prop === 'total-columns'){
+			
+			if(decl.prop === 'create-grid'){
 				var totalColumns = decl.value;
-				var columnClassName = decl.parent.selector + '__';
+				var selectorName = decl.parent.selector + '__';
 				for (i = 1; i <= totalColumns; i++){
 					var width = ((i / totalColumns) * 100).toFixed(5) * 1;
-					decl.parent.parent.append(columnClassName + i + '{width: '+ width + '%; }');
+					decl.parent.parent.append(selectorName + i + '{width: '+ width + '%; }');
 				}
 				decl.parent.remove();
 			}
 		});
+
     };
 });
